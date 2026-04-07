145139cef7a44272e0561e91fa9c2b247861fe6d
AdamMcFlurry <adam@fm5.eu>
2026-04-07 11:43:22 +0200

    la till book

--- src/main/java/se/su/ovning1/Item.java
+++ src/main/java/se/su/ovning1/Item.java
@@ -3,7 +3,7 @@ package se.su.ovning1;
 public class Item implements Priceable {
     private String name;
 
-    public Item(String name) {
+    private Item(String name) {
         this.name = name;
     }
 
