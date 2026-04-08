47c28dc2a300fb3caa86a04778b07910bdf0b8b4
jockelinde <jocke.linde@live.se>
2026-04-08 09:45:43 +0200

    Priceable extends fixes

--- src/main/java/se/su/ovning1/PriceableWithVAT6.java
+++ src/main/java/se/su/ovning1/PriceableWithVAT6.java
@@ -1,6 +1,7 @@
 package se.su.ovning1;
 
-public interface PriceableWithVAT6 {
+public interface PriceableWithVAT6 extends Priceable{
+    @Override
     default double getVAT() {
         return 0.06;
     }
